6c48b5a298afd833742ca35249283fbaa24af2f1
Arjun <82059571+arjunmakesthings@users.noreply.github.com>
2024-08-10 22:06:45 +0530

    Update example.js

--- example.js
+++ example.js
@@ -1,18 +1,22 @@
 //Example for textToPoints; June 2024. 
 
-let lettersToPoints; //Declaring a variable to store all the points. 
+let lettersToPoints; // Declaring a variable to store all the points.
 
 function setup() {
   createCanvas(500, 500);
+  
+  // Now using the method from the addon library
   lettersToPoints = convertLetterToPoints("hello person", 10, height / 2, 3, 85);
 }
 
 function draw() {
   background(255);
   for (let i = 0; i < lettersToPoints.length; i++) {
-    stroke (0);  
-    strokeWeight (0.3) 
-    fill (150); 
-    square(lettersToPoints[i].x, lettersToPoints[i].y+sin(i*frameCount*0.0001)*5, 3);
+    stroke(0);  
+    strokeWeight(0.3);
+    fill(150); 
+    square(lettersToPoints[i].x, lettersToPoints[i].y + sin(i * frameCount * 0.0001) * 5, 3);
   }
 }
+
+
